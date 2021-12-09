f144587777ced1edd724c0b5d78c0e68c9d46b28
guoguomeimei <2021128159@qq.com>
2021-12-08 17:36:37 -0800

    moogoose success

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,9 +1,26 @@
 const express =require("express")
 const app =express()
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+const schema={
+    name:String,
+    age:Number,
+    health:String,
+    score:Number
+}
+const mydata = mongoose.model('cat1s', schema);
+const kitty =new mydata({name:'testzlidjian2'});
+kitty.save()
+
+
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query,"errr")
+    res.send(req.query)
 
     console.log(req.query)
 })
 app.listen(30506)
+
+

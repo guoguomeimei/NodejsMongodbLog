c585f5b7fddbe616c62722bc00755428853d6799
guoguomeimei <2021128159@qq.com>
2021-12-08 17:15:17 -0800

    rjh

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -2,7 +2,8 @@ const express =require("express")
 const app =express()
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    res.send(req.query,"errr")
+
     console.log(req.query)
 })
 app.listen(30506)
